709de53b0ea514053d271c9ddfdf39a7131af641
BerkayEkebas <ekebasahmet@gmail.com>
2025-06-02 13:37:10 +0900

    change

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,37 +1,22 @@
-import LeftBar from "@/components/LeftBar";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import RightBar from "@/components/RightBar";
 
-import type { Metadata } from 'next'
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Lama Dev X Clone',
-  description: 'Next.js social media application project',
-}
+  title: "Lama Dev Social Media App",
+  description: "Social media app built with Next.js",
+};
 
 export default function RootLayout({
   children,
-  modal
 }: Readonly<{
   children: React.ReactNode;
-  modal: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>
-        <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
-          <div className="px-2 xsm:px-4 xxl:px-8 ">
-            <LeftBar />
-          </div>
-          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray ">
-            {children}
-            {modal}
-          </div>
-          <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
-            <RightBar />
-          </div>
-        </div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
