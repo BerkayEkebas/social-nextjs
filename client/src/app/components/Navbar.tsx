04714bfe74579a223c204cbe914a05242bc95173
BerkayEkebas <48918056+BerkayEkebas@users.noreply.github.com>
2025-06-03 20:55:06 +0900

    auth finished

--- client/src/app/components/Navbar.tsx
+++ client/src/app/components/Navbar.tsx
@@ -5,6 +5,7 @@ import MobileMenu from './MobileMenu'
 import Image from 'next/image'
 import { ClerkLoaded, ClerkLoading } from '@clerk/nextjs'
 import { SignedIn, SignedOut } from '@clerk/clerk-react'
+import { UserButton } from '@clerk/nextjs/app-beta'
 
 const Navbar = () => {
   return (
@@ -14,7 +15,7 @@ const Navbar = () => {
         <Link href="/" className='font-bold text-xl text-blue-600'>BERKAYSOCIAL</Link>
       </div>
       {/* CENTER */}
-      <div className='hidden md:flex w-[50%] text-sm'>
+      <div className='hidden md:flex w-[50%] text-sm items-center justify-between'>
         {/* LINKS */}
         <div className='flex gap-6 text-gray-600'>
           <Link className='flex items-center gap-2' href="/">
@@ -30,24 +31,40 @@ const Navbar = () => {
             <span>Stories</span>
           </Link>
         </div>
+        <div className='hidden xl:flex p-2 bg-slate-100 items-center rounded-xl'>
+          <input type='text' placeholder='search..' className='bg-transparent outline-none'/>
+          <Image src="/search.png" width={14} height={14} alt='search'/>
+        </div>
       </div>
       {/* RIGHT */}
       <div className='w-[30%] flex items-center gap-4 xl:gap-8 justify-end'>
         <ClerkLoading>
           <div
-  className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
-  role="status">
-  <span
-    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-    >Loading...</span>
-</div>
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+            role="status">
+            <span
+              className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span>
+          </div>
         </ClerkLoading>
         <ClerkLoaded>
           <SignedIn>
-            Signed In
+            <div className='cursor-pointer'>
+              <Image src="/people.png" alt='' width={24} height={24} />
+            </div>
+            <div className='cursor-pointer'>
+              <Image src="/messages.png" alt='' width={20} height={20} />
+            </div>
+            <div className='cursor-pointer'>
+              <Image src="/notifications.png" alt='' width={20} height={20} />
+            </div>
+            <UserButton/>
           </SignedIn>
           <SignedOut>
-            Signed Out
+            <div className='flex items-center gap-2 text-sm'>
+              <Image src="/login.png" alt='' width={20} height={20} />
+              <Link href="/sign-in">Login/Register</Link>
+            </div>
           </SignedOut>
         </ClerkLoaded>
         <MobileMenu />
